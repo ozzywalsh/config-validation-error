27d690a6487ea88e8d7cab06661047332010f198
Ozzy Walsh <git@ozzy.dev>
2019-12-24 10:19:32 +0000

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      })
+    })
+  ]
+})
+export class AppModule {}
